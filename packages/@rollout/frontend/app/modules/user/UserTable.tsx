69005eea004bf175232250eb4c84ddf9a3d7c8a5
mfrachet <marvin.frachet@strapi.io>
2022-03-30 14:51:40 +0200

    feat(project): add mamber to project

--- packages/@rollout/frontend/app/modules/user/UserTable.tsx
+++ packages/@rollout/frontend/app/modules/user/UserTable.tsx
@@ -1,11 +1,12 @@
-import { Badge, Flex } from "@chakra-ui/react";
+import { Badge, HStack } from "@chakra-ui/react";
 import { useState } from "react";
-import { Form, useTransition } from "remix";
+import { Form, Link, useTransition } from "remix";
 import { Cell, Col, Row, Table, Tbody, Thead } from "../a11y/Table";
 import { useHydrated } from "../misc/useHydrated";
 import { UserProject, UserRoles } from "../projects/types";
 import { FaTrash } from "react-icons/fa";
 import { Button } from "~/components/Button";
+import { IoIosCreate } from "react-icons/io";
 
 export interface UserTableProps {
   userProjects: Array<UserProject>;
@@ -39,21 +40,33 @@ export const UserTable = ({
       <input type="hidden" name="_method" value="delete-member" />
 
       {canEdit && (
-        <Flex justifyContent={["center", "flex-end"]}>
+        <HStack mb={[6, 2]} spacing={[0, 4]} flexDirection={["column", "row"]}>
+          <Button
+            colorScheme="brand"
+            as={Link}
+            to={"/"}
+            leftIcon={<IoIosCreate aria-hidden />}
+            variant="outline"
+            width={["100%", "auto"]}
+            mb={[2, 0]}
+          >
+            Add member
+          </Button>
+
           <Button
             colorScheme="error"
             type={canDelete || !isHydrated ? "submit" : "button"}
             aria-disabled={isHydrated && !canDelete}
             leftIcon={<FaTrash aria-hidden />}
             variant="outline"
-            mb={[6, 2]}
             isLoading={transition.state === "submitting"}
             loadingText="Deleting the member(s), please wait..."
             disabled={false}
+            width={["100%", "auto"]}
           >
             Remove from project
           </Button>
-        </Flex>
+        </HStack>
       )}
 
       <Table labelledBy={labelledBy} onSelect={setSelected} selected={selected}>
