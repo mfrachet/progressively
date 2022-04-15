7bd4912b9e8e39ecd7ab08daa9edb15a64465b44
mfrachet <marvin.frachet@strapi.io>
2022-04-15 14:32:52 +0200

    go for it

--- packages/react/webpack.config.js
+++ packages/react/webpack.config.js
@@ -19,7 +19,39 @@ const legacyConfig = {
   },
   output: {
     filename: "index.js",
-    path: path.resolve(__dirname, "lib"),
+    path: path.resolve(__dirname, "lib", "legacy"),
+    globalObject: "this",
+    library: {
+      name: "@rollout/react",
+      type: "umd",
+    },
+  },
+  externals: {
+    react: "react",
+  },
+};
+
+const modernConfig = {
+  entry: "./src/index.tsx",
+  mode: "production",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: "ts-loader",
+        exclude: /node_modules/,
+        options: {
+          configFile: "tsconfig.modern.json",
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  output: {
+    filename: "index.js",
+    path: path.resolve(__dirname, "lib", "modern"),
     globalObject: "this",
     library: {
       name: "@rollout/react",
@@ -64,4 +96,4 @@ const ssrConfig = {
   },
 };
 
-module.exports = [legacyConfig, ssrConfig];
+module.exports = [legacyConfig, modernConfig, ssrConfig];
