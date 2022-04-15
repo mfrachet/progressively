e9fa30c948ee8d6aa839e5afbeb402f1d5c9d55e
mfrachet <marvin.frachet@strapi.io>
2022-04-15 11:31:43 +0200

    refactor(react): split ssr / csr libraries

--- packages/react/webpack.config.js
+++ packages/react/webpack.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 const BundleAnalyzerPlugin =
   require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 
-module.exports = {
+const legacyConfig = {
   entry: "./src/index.tsx",
   mode: "production",
   module: {
@@ -34,3 +34,34 @@ module.exports = {
       ? [new BundleAnalyzerPlugin()]
       : undefined,
 };
+
+const ssrConfig = {
+  entry: "./src/ssr.ts",
+  mode: "production",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  output: {
+    filename: "ssr.js",
+    path: path.resolve(__dirname, "lib"),
+    globalObject: "this",
+    library: {
+      name: "@rollout/react",
+      type: "commonjs",
+    },
+  },
+  externals: {
+    react: "react",
+  },
+};
+
+module.exports = [legacyConfig, ssrConfig];
