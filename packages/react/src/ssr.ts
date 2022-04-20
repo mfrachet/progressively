49a59a4bd0cd3867a860e6e06180d86fbbd2a6cb
mfrachet <marvin.frachet@strapi.io>
2022-04-20 07:48:28 +0200

    chore(renaming): rollout (already used) to progressively

--- packages/react/src/ssr.ts
+++ packages/react/src/ssr.ts
@@ -1,11 +1,10 @@
 import ProgressivelySdk, { SDKOptions } from "@progressively/sdk-js";
-import { ProgressivelyProviderProps } from "./types";
 
 export function getSSRProps(
   clientKey: string,
   options?: SDKOptions | undefined
 ) {
-  const sdk = ProgressivelytSdk.init(clientKey, options);
+  const sdk = ProgressivelySdk.init(clientKey, options);
 
   return sdk.loadFlags().then((initialFlags) => ({
     initialFlags,
