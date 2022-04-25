259304d2fd0ebef5fab8fdd4e90ffdf0607f4da0
mfrachet <marvin.frachet@strapi.io>
2022-04-25 09:10:02 +0200

    fix tests

--- packages/backend/src/main.ts
+++ packages/backend/src/main.ts
@@ -6,11 +6,11 @@ import { WsAdapter } from '@nestjs/platform-ws';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    cors: {
-      origin: ['http://localhost:3000', 'http://localhost:3001'],
-      credentials: true,
-    },
+  const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    credentials: true,
+    origin: true,
   });
 
   // Middlewares
