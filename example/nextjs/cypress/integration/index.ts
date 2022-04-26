159b7574402e71f34b6a007600c328d5bbbb9409
mfrachet <marvin.frachet@strapi.io>
2022-04-26 06:36:29 +0200

    more e2e tests for nextjs example

--- example/nextjs/cypress/integration/index.ts
+++ example/nextjs/cypress/integration/index.ts
@@ -11,9 +11,12 @@ describe("/", () => {
   it("shows the old variant when the flag is not activated", () => {
     cy.findByText("Old variant").should("be.visible");
     cy.findByText("New variant").should("not.exist");
+
+    cy.findByText("New footer").should("be.visible");
+    cy.findByText("Old footer").should("not.exist");
   });
 
-  it("shows the new variant when the flag is activated", () => {
+  it("shows the new variant when the flag is activated for homepage", () => {
     cy.changeFlagStatus("1", "1", "1", FlagStatus.ACTIVATED);
 
     // Verify the activation using sockets
@@ -24,5 +27,18 @@ describe("/", () => {
     cy.reload();
     cy.findByText("Old variant").should("not.exist");
     cy.findByText("New variant").should("be.visible");
+  });
+
+  it("shows the old variant when the flag is activated for footer (by default activated)", () => {
+    cy.changeFlagStatus("1", "1", "2", FlagStatus.NOT_ACTIVATED);
+
+    // Verify the activation using sockets
+    cy.findByText("New variant").should("not.exist");
+    cy.findByText("Old variant").should("be.visible");
+
+    // Verify the activation using SSR
+    cy.reload();
+    cy.findByText("New footer").should("not.exist");
+    cy.findByText("Old footer").should("be.visible");
   });
 });
