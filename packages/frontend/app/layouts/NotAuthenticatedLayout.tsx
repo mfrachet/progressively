7e630ac15845f44d292773575f161f674be243e9
mfrachet <marvin.frachet@strapi.io>
2022-05-03 15:40:15 +0200

    feat(welcome): add welcome page to create admin user

--- packages/frontend/app/layouts/NotAuthenticatedLayout.tsx
+++ packages/frontend/app/layouts/NotAuthenticatedLayout.tsx
@@ -9,17 +9,20 @@ export const NotAuthenticatedLayout = ({
 }: NotAuthenticatedLayoutProps) => {
   return (
     <div>
-      <Container maxW="5xl">
-        <Flex
-          p={3}
-          as={"nav"}
-          aria-label="General"
-          justifyContent={"space-between"}
-          alignItems="center"
-        >
-          <Logo to="/" />
-        </Flex>
-      </Container>
+      <Box borderBottomWidth={1} borderBottomColor="text.100">
+        <Container maxW="5xl">
+          <Flex
+            py={3}
+            as={"nav"}
+            aria-label="General"
+            justifyContent={"space-between"}
+            alignItems="center"
+            h={"72px"}
+          >
+            <Logo to="/" />
+          </Flex>
+        </Container>
+      </Box>
 
       <Container maxW="5xl">
         <Box maxW="65ch" pt={16} pb={4}>
