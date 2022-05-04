64362f69612950c7a83cc673a91dbf70d2b09ea8
mfrachet <marvin.frachet@strapi.io>
2022-05-04 17:45:51 +0200

    docs(revamp): new documentation

--- packages/documentation/src/components/HomepageFeatures/index.js
+++ packages/documentation/src/components/HomepageFeatures/index.js
@@ -1,43 +1,65 @@
-import React from 'react';
-import clsx from 'clsx';
-import styles from './styles.module.css';
+import React from "react";
+import clsx from "clsx";
+import styles from "./styles.module.css";
 
 const FeatureList = [
   {
-    title: 'Easy to Use',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+    title: "Lightweight",
+    Svg: require("@site/static/img/undraw_docusaurus_mountain.svg").default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        Progressively provides lightweight SDKs so that your websites and
+        applications don't suffer from performance issues. <br />
+        <br />
+        <div
+          style={{
+            display: "flex",
+          }}
+        >
+          <span style={{ fontSize: "10px", textTransform: "uppercase" }}>
+            React SDK:{" "}
+            <img
+              src="https://img.shields.io/bundlephobia/minzip/@progressively/react"
+              alt="React SDK minified and gzipped size"
+            />
+          </span>
+          <span style={{ fontSize: "10px", textTransform: "uppercase" }}>
+            JavaScript SDK:{" "}
+            <img
+              src="https://img.shields.io/bundlephobia/minzip/@progressively/sdk-js"
+              alt="JS SDK minified and gzipped size"
+            />
+          </span>
+        </div>
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
-    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
+    title: "Accessible",
+    Svg: require("@site/static/img/undraw_docusaurus_tree.svg").default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        Progressively's dashboard has been built with accessibility in mind and
+        will always be. We won't make tradeoffs impacting negatively our users.
       </>
     ),
   },
   {
-    title: 'Powered by React',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+    title: "Tracking free",
+    Svg: require("@site/static/img/undraw_docusaurus_react.svg").default,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        Progressively does not track anything related to users activities. The
+        only metric that we get are the flag hits so that we can provide
+        insights and charts about the impact of a flag switch.
       </>
     ),
   },
 ];
 
-function Feature({Svg, title, description}) {
+function Feature({ Svg, title, description }) {
   return (
-    <div className={clsx('col col--4')}>
+    <div className={clsx("col col--4")}>
       <div className="text--center">
         <Svg className={styles.featureSvg} role="img" />
       </div>
