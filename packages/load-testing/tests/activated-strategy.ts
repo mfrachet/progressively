cebb859f39dcc700ff29f476e72e278b91efe4a6
mfrachet <marvin.frachet@strapi.io>
2022-05-12 13:42:16 +0200

    working with different browsers

--- packages/load-testing/tests/activated-strategy.ts
+++ packages/load-testing/tests/activated-strategy.ts
@@ -1,13 +1,18 @@
+import { LocalBrowserType } from "./helpers/BrowserChain";
 import { changeFlagStatus } from "./helpers/changeFlagStatus";
 import { expect, test, TestConfig } from "./helpers/hooks";
 
+const userCount = process.env.USER_COUNT;
+const browserType = process.env.BROWSER_TYPE as LocalBrowserType;
+
 const config: TestConfig = {
-  userCount: 2,
-  type: "chromium",
+  userCount: userCount ? Number(userCount) : 2,
+  type: browserType || "chromium",
 };
 
 test("percentage based rollout", config, async (browsers) => {
   await browsers.open("http://localhost:3000");
+
   await browsers.run(async (page, index) => {
     await expect(page).toHaveText(`User ${index}`);
     await expect(page).toHaveText(`New variant`);
