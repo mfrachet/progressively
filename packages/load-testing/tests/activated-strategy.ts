97f7670eac52856ffa0dd0eed4ecfbb099b74364
mfrachet <marvin.frachet@strapi.io>
2022-05-12 09:57:43 +0200

    making it app-aware

--- packages/load-testing/tests/activated-strategy.ts
+++ packages/load-testing/tests/activated-strategy.ts
@@ -1,15 +1,13 @@
-import { test } from "./helpers/hooks";
+import { test, TestConfig } from "./helpers/hooks";
 
-const userCount = 2;
+const config: TestConfig = {
+  userCount: 2,
+  type: "chromium",
+};
 
-test(
-  "percentage based rollout",
-  userCount,
-  "chromium",
-  async (browserChain) => {
-    await browserChain.openForAll("http://localhost:3000");
-    await browserChain.run(async (page, index) => {
-      await await page.$$(`text='User ${index}'`);
-    });
-  }
-);
+test("percentage based rollout", config, async (browserChain) => {
+  await browserChain.openForAll("http://localhost:3000");
+  await browserChain.run(async (page, index) => {
+    await await page.$$(`text='User ${index}'`);
+  });
+});
--- packages/load-testing/tests/helpers/hooks.ts
+++ packages/load-testing/tests/helpers/hooks.ts
@@ -1,16 +1,24 @@
 import { BrowserChain, LocalBrowserType } from "./BrowserChain";
 import { seedDb, cleanupDb } from "../../src/seed";
 
+export interface TestConfig {
+  userCount: number;
+  type: LocalBrowserType;
+}
+
 export const test = async (
   name: string,
-  userCount: number,
-  type: LocalBrowserType,
+  config: TestConfig,
   fn: (browserChain: BrowserChain) => Promise<unknown>
 ) => {
-  await seedDb(userCount);
+  await seedDb(config.userCount);
 
   try {
-    const browserChain = await BrowserChain.create(userCount, type);
+    const browserChain = await BrowserChain.create(
+      config.userCount,
+      config.type
+    );
+
     await fn(browserChain);
     await browserChain.closeAll();
   } catch (error) {
