3d715b4b76fbe606e0ef1d8a11b8d55578cb44e3
mfrachet <marvin.frachet@strapi.io>
2022-05-13 07:04:44 +0200

    by percent

--- packages/load-testing/tests/activated-strategy.ts
+++ packages/load-testing/tests/activated-strategy.ts
@@ -2,36 +2,37 @@ import { createCounter, expect } from "./helpers/assertions";
 import { LocalBrowserType } from "./helpers/BrowserChain";
 import { changeFlagStatus } from "./helpers/changeFlagStatus";
 import { test, TestConfig } from "./helpers/hooks";
+import { seedDb, cleanupDb } from "./helpers/seed";
 
 const userCount = process.env.USER_COUNT;
 const browserType = process.env.BROWSER_TYPE as LocalBrowserType;
 
 const config: TestConfig = {
-  userCount: userCount ? Number(userCount) : 10,
+  userCount: userCount ? Number(userCount) : 20,
   type: browserType || "chromium",
 };
 
-// test(
-//   "[Boolean activation]: everybody receives the same variations at load and update time",
-//   config,
-//   async (browsers) => {
-//     await browsers.open("http://localhost:3000");
+const t1 = test(
+  "[Boolean activation]: everybody receives the same variations at load and update time",
+  config,
+  async (browsers) => {
+    await browsers.open("http://localhost:3000");
 
-//     await browsers.run(async (page, index) => {
-//       await expect(page).toHaveText(`User ${index}`);
-//       await expect(page).toHaveText(`New variant`);
-//     });
+    await browsers.run(async (page, index) => {
+      await expect(page).toHaveText(`User ${index}`);
+      await expect(page).toHaveText(`New variant`);
+    });
 
-//     await changeFlagStatus("1", "1", "NOT_ACTIVATED");
+    await changeFlagStatus("1", "1", "NOT_ACTIVATED");
 
-//     await browsers.run(async (page, index) => {
-//       await expect(page).toHaveText(`User ${index}`);
-//       await expect(page).toHaveText(`Old variant`);
-//     });
-//   }
-// );
+    await browsers.run(async (page, index) => {
+      await expect(page).toHaveText(`User ${index}`);
+      await expect(page).toHaveText(`Old variant`);
+    });
+  }
+);
 
-test(
+const t2 = test(
   "[Percentage activation]: 25% of the people should receive the activated variant when activating it",
   config,
   async (browsers) => {
@@ -55,12 +56,23 @@ test(
       config.userCount
     );
 
-    await browsers.run(async (page, index) => {
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+
+    await browsers.run(async (page) => {
       await activatedCounter.increaseWhenVisible(page);
       await notActivatedCounter.increaseWhenVisible(page);
     });
 
     await activatedCounter.verifyRange(15, 20);
-    await notActivatedCounter.verifyRange(80, 100);
+    await notActivatedCounter.verifyRange(75, 85);
   }
 );
+
+async function run() {
+  await seedDb(config.userCount);
+  await t1();
+  await t2();
+  await cleanupDb();
+}
+
+run();
--- packages/load-testing/tests/helpers/hooks.ts
+++ packages/load-testing/tests/helpers/hooks.ts
@@ -1,33 +1,29 @@
 import chalk from "chalk";
-import { Page } from "playwright";
 import { BrowserChain, LocalBrowserType } from "./BrowserChain";
-import { seedDb, cleanupDb } from "./seed";
 
 export interface TestConfig {
   userCount: number;
   type: LocalBrowserType;
 }
 
-export const test = async (
-  name: string,
-  config: TestConfig,
-  fn: (browserChain: BrowserChain) => Promise<unknown>
-) => {
-  await seedDb(config.userCount);
+export const test =
+  (
+    name: string,
+    config: TestConfig,
+    fn: (browserChain: BrowserChain) => Promise<unknown>
+  ) =>
+  async () => {
+    try {
+      const browserChain = await BrowserChain.create(
+        config.userCount,
+        config.type
+      );
 
-  try {
-    const browserChain = await BrowserChain.create(
-      config.userCount,
-      config.type
-    );
-
-    await fn(browserChain);
-    await browserChain.closeAll();
-    console.info(chalk.green(`✓ ${name}`));
-  } catch (error) {
-    console.error(chalk.red(`FAILED: ${name}\n`));
-    throw error;
-  } finally {
-    await cleanupDb();
-  }
-};
+      await fn(browserChain);
+      await browserChain.closeAll();
+      console.info(chalk.green(`✓ ${name}`));
+    } catch (error) {
+      console.error(chalk.red(`FAILED: ${name}\n`));
+      throw error;
+    }
+  };
