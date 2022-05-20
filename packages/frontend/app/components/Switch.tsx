8ad4ff4e0e585fd540e3a033b37307520a020401
mfrachet <marvin.frachet@strapi.io>
2022-05-20 09:52:57 +0200

    refacto(chakra): remove missing chakra things

--- packages/frontend/app/components/Switch.tsx
+++ packages/frontend/app/components/Switch.tsx
@@ -1,26 +1,8 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { HTMLAttributes, useState } from "react";
-import styled from "@emotion/styled";
+
 import { Typography } from "./Typography";
 import { Spinner } from "./Spinner";
-
-const RawSwitch = styled.div`
-  transition: all 0.3s;
-  position: relative;
-
-  &:before {
-    transition: all 0.3s;
-    position: absolute;
-    width: ${({ theme }: any) => theme.sizes[6]};
-    height: ${({ theme }: any) => theme.sizes[6]};
-    border-radius: 50%;
-    content: "";
-    background: ${({ theme }: any) => theme.colors.white};
-    transform: translateX(
-      ${(props) => (props["data-is-checked"] ? "0%" : "-100%")}
-    );
-  }
-`;
 
 export interface SwitchProps extends HTMLAttributes<HTMLButtonElement> {
   checked: boolean;
@@ -40,9 +22,9 @@ export const Switch = ({ checked, optimistic, ...props }: SwitchProps) => {
       {...props}
     >
       <Typography as="span">Off</Typography>
-      <RawSwitch aria-hidden data-is-checked={internalChecked}>
+      <div aria-hidden data-is-checked={internalChecked}>
         {optimistic && <Spinner />}
-      </RawSwitch>
+      </div>
       <Typography as="span">On</Typography>
     </button>
   );
